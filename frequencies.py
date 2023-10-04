70a4816a4e3f0561f4b1d2d4788e0dea89171428
Emile Jurgens <emile.jurgens@kcl.ac.uk>
2023-10-04 19:25:03 +0100

    frequencies solution

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,12 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        if type(item) != "String":
+            item = str(item)
+        if item in frequencies:
+            frequencies[item] += 1
+        else:
+            frequencies[item] = 1
     return frequencies
+
